32c11e3ecd49be7714bb3ac2d63304b9e05d1b73
Angelo Miranda <a.miranda01084@student.tsu.edu.ph>
2024-11-13 01:16:55 +0800

    footer, additional files, and profile section

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const navLinks = document.getElementById('nav-links');
 const navMenuButton = document.getElementById('nav-menu-button');
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
 
 navMenuButton.addEventListener('click', () => {
     navLinks.classList.toggle('show');
+
+    if (navLinks.classList.contains('show')) {
+        const totalNavHeight = nav.offsetHeight + navLinks.offsetHeight;
+        header.style.paddingTop = `${totalNavHeight}px`;
+    } else {
+        header.style.paddingTop = '100px';
+    }
 });
 
 window.addEventListener('resize', () => {
     if (window.innerWidth > 980) {
         navLinks.classList.remove('show');
+        header.style.paddingTop = '100px'; 
     }
 });
